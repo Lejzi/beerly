4726763609a9e61fb4316367af396d94055cce29
Krzysztof Siwak <krzysztof.siwak1@gmail.com>
2021-07-23 19:53:34 +0200

    update: add collapse function and change meta tags

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,9 +11,19 @@ const Accordion = ({ data }) => {
 		console.log('opening', id);
 		setList([...newList]);
 	};
+	const handleCollapse = () => {
+		const newList = list;
+
+		newList.forEach((beer, index) => {
+			newList[index].open = false;
+		});
+
+		setList([...newList]);
+	};
 
 	return (
 		<div className='accordion'>
+			<button onClick={() => handleCollapse()}>Collapse all</button>
 			{list &&
 				list.map((item, index) => {
 					return (
--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const ListCard = ({ data, action }) => {
 	return (
-		<div className={`list-card ${data.open ? 'open' : null}`}>
+		<div
+			className={`list-card ${data.open ? 'open' : null}`}
+			onClick={() => action()}
+		>
 			<div className='top'>
 				<h3>{data.name}</h3>
-				<button className='carret' onClick={() => action()}>
-					^
-				</button>
+				<button className='carret'>^</button>
 			</div>
 			<div className='list-details'>
 				<p>{data.tagline}</p>
