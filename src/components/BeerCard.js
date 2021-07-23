f05fa20604e1dfeabf7726d092274a7db4a87aa1
Krzysztof Siwak <krzysztof.siwak1@gmail.com>
2021-07-23 22:29:09 +0200

    feature: add cta cards and add filter functions

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -3,7 +3,12 @@ import React from 'react';
 const BeerCard = ({ data }) => {
 	return (
 		<div className='beer-card'>
-			<p>beer card</p>
+			<img src={data.image_url} alt='beer-image' />
+			<h3>{data.name}</h3>
+			<p>{data.tagline}</p>
+			<p>vol. {data.abv}</p>
+			<p>{data.description}</p>
+			<a href='#'>More Info {'>'}</a>
 		</div>
 	);
 };
--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,11 +1,64 @@
-import React from 'react';
 import React, { useState, useEffect } from 'react';
-const [list, setList] = useState(data);
+import BeerCard from './BeerCard';
 
 const Cta = ({ data }) => {
+	const [list, setList] = useState(data);
+	const [search, setSearch] = useState('');
+	const [filter, setFilter] = useState('');
+
+	const handleSearch = (e) => {
+		const value = e.target.value;
+
+		const newList = data.filter((item) =>
+			item.name.toUpperCase().includes(value.toUpperCase())
+		);
+
+		setList([...newList]);
+	};
+
+	const handleFilter = (e) => {
+		const filter = e.target.value;
+		const newList = list;
+
+		switch (filter) {
+			case 'none':
+				break;
+			case 'asc':
+				newList.sort((firstItem, secondItem) => firstItem.abv - secondItem.abv);
+				break;
+			case 'desc':
+				newList.sort((firstItem, secondItem) => secondItem.abv - firstItem.abv);
+		}
+
+		setList([...newList]);
+	};
+
 	return (
-		<div className='cta'>
-			<p>Cta hello</p>
+		<div className='ctas'>
+			<input
+				type='text'
+				name='search'
+				className='search-field'
+				placeholder='Search'
+				onChange={(e) => handleSearch(e)}
+			/>
+			<select
+				className='search-field'
+				name='filter'
+				onChange={(e) => {
+					handleFilter(e);
+				}}
+			>
+				<option value='none'>Sort</option>
+				<option value='asc'>vol. asc</option>
+				<option value='desc'>vol. desc</option>
+			</select>
+			<div className='cta'>
+				{list &&
+					list.map((item) => {
+						return <BeerCard key={item.id} data={item} />;
+					})}
+			</div>
 		</div>
 	);
 };
